48e226bce57cd776216ffc0cdb9c2a48d01cd2e6
DavideCruciani <cruciani240203@gmail.com>
2023-03-12 03:16:35 +0100

    First 2 section of the of the main component done

--- final-project-app/src/App.js
+++ final-project-app/src/App.js
@@ -3,7 +3,12 @@ import Header from './components/Header';
 import Nav from './components/Nav';
 import Main from './components/Main';
 import Footer from './components/Footer';
-import Logo from './images/Logo.svg'
+import Logo from './images/Logo.svg';
+import RestaurantFood from './images/restauranfood.jpg'
+import FirstCardImg from './images/greeksalad.jpg'
+import SecondCardImg from './images/bruchetta.svg'
+import ThirdCardImg from './images/lemondessert.jpg'
+
 
 function App() {
   return (
@@ -11,7 +16,7 @@ function App() {
       <Header img={Logo}>
         <Nav/>
       </Header>
-      <Main></Main>
+      <Main img={RestaurantFood} cardimg1={FirstCardImg} cardimg2={SecondCardImg} cardimg3={ThirdCardImg}></Main>
       <Footer/>
     </>
   );
--- final-project-app/src/components/Main.js
+++ final-project-app/src/components/Main.js
@@ -1,5 +1,74 @@
-export default function Main() {
+export default function Main(props) {
   return (
-    <main></main>
+    <main>
+      <section>
+        <div>
+          <h1>Little Lemon</h1>
+          <h3>Chicago</h3>
+          <p>We are a family owned Mediterranean restaurant, focused on traditional <br/> recipes served with a modern twist.</p>
+          <button>Reserve a Table</button>
+        </div>
+        <div>
+          <img src={props.img} alt="Restaurant food"></img>
+        </div>
+      </section>
+      <section>
+        <div className="week-specials">
+          <h2>This week specials!</h2>
+          <button>Online Menu</button>
+        </div>
+        <div className="cards-container">
+
+          <article className="Card">
+            <img src={props.cardimg1}></img>
+            <div className="dish-info">
+              <div className="dish-title">
+                <h4>Greek salad</h4>
+                <p className="price">$12.99</p>
+              </div>
+              <div>
+                <p className="dish-desc">The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.</p>
+              </div>
+              <div>
+                <p className="order-delivery">Order a delivery</p>
+              </div>
+            </div>
+          </article>
+
+          <article className="Card">
+          <img src={props.cardimg2}></img>
+            <div className="dish-info">
+              <div className="dish-title">
+                <h4>Greek salad</h4>
+                <p className="price">$12.99</p>
+              </div>
+              <div>
+                <p className="dish-desc">The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.</p>
+              </div>
+              <div>
+                <p className="order-delivery">Order a delivery</p>
+              </div>
+            </div>
+          </article>
+
+          <article className="Card">
+          <img src={props.cardimg3}></img>
+            <div className="dish-info">
+              <div className="dish-title">
+                <h4>Greek salad</h4>
+                <p className="price">$12.99</p>
+              </div>
+              <div>
+                <p className="dish-desc">The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.</p>
+              </div>
+              <div>
+                <p className="order-delivery">Order a delivery</p>
+              </div>
+            </div>
+          </article>
+
+        </div>
+      </section>
+    </main>
   )
 }
